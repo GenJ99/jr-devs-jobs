e9f2814f4ebc5db6b4e20572d2168ea1549c53db
GenJay99 <jasongencarella1@gmail.com>
2019-12-10 14:59:58 -0500

    Iterate Through the Job Pages

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch");
+
+const baseURL = "https://jobs.github.com/positions.json";
+
+module.exports = async function fetchGithub() {
+  const res = await fetch(baseURL);
+  const jobs = await res.json();
+  console.log({ jobs });
+
+  console.log(jobs.length);
+};
+
+module.exports();
