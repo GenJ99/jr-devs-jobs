1db352b92833ae16451ee80589bb907003487beb
GenJay99 <jasongencarella1@gmail.com>
2019-12-10 12:05:08 -0500

    Create Front-End Part 2 with JSON Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
+// COMPONENTS
+import Jobs from "./components/Jobs";
+
+const mockJobs = [
+  { title: "SWE 1", company: "Google" },
+  { title: "SWE 1", company: "Facbook" },
+  { title: "SWE 1", company: "Apple" }
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Jobs jobs={mockJobs} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/jobs.js
+++ client/src/components/jobs.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 
-export default function Jobs() {
-  return <div></div>;
+// COMPONENTS
+import Job from "./Job";
+
+export default function Jobs({ jobs }) {
+  return (
+    <div className="jobs">
+      <Typography variant="h1">Entry Level Sofware Jobs</Typography>
+
+      {jobs.map(job => (
+        <Job job={job} />
+      ))}
+    </div>
+  );
 }
