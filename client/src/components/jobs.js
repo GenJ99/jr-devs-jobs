247d569df9d9c8c061558be137b080fcf6331d55
GenJay99 <jasongencarella1@gmail.com>
2019-12-11 18:33:09 -0500

    Style CSS, Add Material-UI/Java Objects to Jobs.js

--- client/src/components/jobs.js
+++ client/src/components/jobs.js
@@ -5,9 +5,13 @@ import Typography from "@material-ui/core/Typography";
 import Job from "./Job";
 
 export default function Jobs({ jobs }) {
+  console.log("job is ", jobs[0]);
+
   return (
     <div className="jobs">
-      <Typography variant="h1">Entry Level Sofware Jobs</Typography>
+      <Typography variant="h4" component="h1">
+        Entry Level Sofware Jobs
+      </Typography>
 
       {jobs.map(job => (
         <Job job={job} />
